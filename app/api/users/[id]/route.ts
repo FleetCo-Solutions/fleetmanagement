1eb91e98e7ad894aaaee6c05dc4118164fe4ced6
heismoody <moodyamin531@gmail.com>
2025-12-05 17:01:45 +0300

    route api for the dver details

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,8 +1,15 @@
 // @ts-nocheck
 import { NextRequest } from "next/server";
 import { getUserDetails } from "./get";
+import { editUser } from "./put";
 
 export async function GET(_request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     return getUserDetails(id);
+}
+
+export async function PUT(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const body = await request.json();
+    return editUser(id, body);
 }
